07c43518120eda4c6a4c1af7a28637392691c0c0
mrmp <mohamadpouran@gmail.com>
2022-01-30 22:30:48 +0330

    make search in postsn title

--- app/Http/Controllers/Panel/PostController.php
+++ app/Http/Controllers/Panel/PostController.php
@@ -17,13 +17,26 @@ class PostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         if (auth()->user()->role === 'author') {
-            $posts = Post::where('user_id', auth()->user()->id)->with('user')->paginate();
+            $postsQuery = Post::where('user_id', auth()->user()->id)->with('user');
+
+            if ($request->search) {
+                $postsQuery->where('title', 'LIKE', "%{$request->search}%");
+            }
+
+            $posts = $postsQuery->paginate();
         } else {
-            $posts = Post::with('user')->paginate();
+            $postsQuery = Post::with('user');
+
+            if ($request->search) {
+                $postsQuery->where('title', 'LIKE', "%{$request->search}%");
+            }
+
+            $posts = $postsQuery->paginate();
         }
+
 
         return view('panel.posts.index', compact('posts'));
     }
