e1099f8918e269c1e5654879f2dccfffbc439b66
mrmp <mohamadpouran@gmail.com>
2022-01-31 18:12:22 +0330

    dissplay category in menu with view component

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'Asia/Tehran',
+    'timezone' => 'UTC',
 
     /*
     |--------------------------------------------------------------------------
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\ViewServiceProvider::class,
 
     ],
 
