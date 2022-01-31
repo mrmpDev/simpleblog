789db657dbd5d78fd28a7414823559b6eceb93ad
mrmp <mohamadpouran@gmail.com>
2022-01-31 12:50:52 +0330

    implementing comment management with display comment, delete comment, approve comment, show comment replies and count

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,38 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'content',
+        'is_approved',
+        'user_id',
+        'comment_id',
+        'post_id'
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function getCreatedAtInJalali()
+    {
+        return verta($this->created_at)->format('Y/m/d');
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function getStatusInFarsi()
+    {
+        return !!$this->is_approved ? 'تایید شده' : 'تایید نشده';
+    }
 }
